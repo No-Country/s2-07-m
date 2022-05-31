f1d69fd5f66f6a58cc1b385a6030907410fa2cc2
nicolagardo <nicolagardo@gmail.com>
2022-05-31 13:49:47 -0300

    organizando carpeta client

---
+++ client/src/components/Inicio.js
@@ -0,0 +1,16 @@
+import './css/Inicio.css'
+
+
+export const Inicio = () => {
+
+
+  return (
+    <div className='inicio-body'>
+      <h1>
+        Inicio
+      </h1>
+      <p>Nulla tempor deserunt deserunt est excepteur exercitation do aute ex deserunt voluptate eiusmod.Nulla tempor deserunt deserunt est excepteur exercitation do aute ex deserunt voluptate eiusmod.</p>
+    </div>
+  )
+ 
+}
