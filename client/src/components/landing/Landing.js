bcdeafca584ec844c35133bc1c60afb72c9c4239
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-06-07 00:25:32 -0300

    temp(landing): assets and landing

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,3 +1,7 @@
+import { Beneficios } from './Beneficios'
+import { Comercios } from './Comercios'
+import { Inicio } from './Inicio'
+import {Servicios} from './Servicios'
 import './styles/landing.css'
 
 
@@ -6,10 +10,15 @@ export const Landing = () => {
 
   return (
     <div className='inicio-body'>
-      <h1>
+     <Inicio /> 
+      
+     
+     
+     
+      {/* <h1>
         Inicio
       </h1>
-      <p>Nulla tempor deserunt deserunt est excepteur exercitation do aute ex deserunt voluptate eiusmod.Nulla tempor deserunt deserunt est excepteur exercitation do aute ex deserunt voluptate eiusmod.</p>
+      <p>Nulla tempor deserunt deserunt est excepteur exercitation do aute ex deserunt voluptate eiusmod.Nulla tempor deserunt deserunt est excepteur exercitation do aute ex deserunt voluptate eiusmod.</p> */}
     </div>
   )
  
